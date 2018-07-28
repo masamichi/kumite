9162c3bd67e2da9a7534a8243e02dcad08b0b20b
FujisawaMasamichi <FujisawaMasamicih@lifull.com>
2018-07-29 07:55:15 +0900

    Add TeaManager to Tea model.

--- apps/menu/models.py
+++ apps/menu/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+TEA_KINDS = (
+    ("english", "英国紅茶"),
+    ("chinese", "中国茶"),
+    ("japanese", "日本茶")
+)
+
+class TeaManager(models.Manager):
+    def count_each_kind(self):
+        result = self.values_list("kind").annotate(
+            count=models.Count("kind")
+        )
+        return dict(result)
+
